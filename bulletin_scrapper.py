841b48c084abc2d07a5f129b085a9cb8ef7b0215
Mekhi Rhodie <mekhirhodie@gmail.com>
2025-02-06 00:07:19 -0500

    Update bulletin_scrapper.py
    - add a function to scrape the table data from the specified URL. Preferably https://www.cisa.gov/news-events/bulletins/sb25-034

--- bulletin_scrapper.py
+++ bulletin_scrapper.py
@@ -4,3 +4,10 @@ import requests
 from bs4 import BeatifulSoup
 from tabulate import tabulate
 
+def scrap_cisa_table(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except Exception as e:
+        print(f"Error fetching URL: {e}")
+        return
